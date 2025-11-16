6a6a6d325eedac169aa3220892d5722de8efe7c6
petarzarkov <pzarko1@gmail.com>
2025-11-16 12:55:13 +0200

    feat: add some swing

--- src/audio_renderer.rs
+++ src/audio_renderer.rs
@@ -4,10 +4,16 @@ use super::beat_maker::DrumSound;
 type AudioSample = i16;
 const SAMPLE_RATE: u32 = 44100;
 
-const MILLISECONDS_PER_STEP: f32 = 250.0;
+const MILLISECONDS_PER_STEP: f32 = 250.0; 
 
-// At 44.1kHz, that is 44100 * 0.25 = 11,025 samples.
-const SAMPLES_PER_STEP: u32 = (SAMPLE_RATE as f32 * (MILLISECONDS_PER_STEP / 1000.0)) as u32;
+const SWING_OFFSET_MS: f32 = 80.0; 
+
+const SAMPLES_PER_STEP_BASE: u32 = (SAMPLE_RATE as f32 * (MILLISECONDS_PER_STEP / 1000.0)) as u32;
+const SWING_OFFSET_SAMPLES: u32 = (SAMPLE_RATE as f32 * (SWING_OFFSET_MS / 1000.0)) as u32;
+
+const ON_BEAT_STEP_LENGTH: u32 = SAMPLES_PER_STEP_BASE + SWING_OFFSET_SAMPLES;
+const OFF_BEAT_STEP_LENGTH: u32 = SAMPLES_PER_STEP_BASE - SWING_OFFSET_SAMPLES;
+
 
 pub fn render_beat_to_wav(beat: &[DrumSound], filename: &str) -> Result<(), hound::Error> {
     let mut kick_reader = hound::WavReader::open("samples/kick.wav")?;
@@ -26,11 +32,19 @@ pub fn render_beat_to_wav(beat: &[DrumSound], filename: &str) -> Result<(), houn
         .samples::<AudioSample>()
         .collect::<Result<Vec<_>, _>>()?;
     
-    // This assumes all files are 44.1kHz, 16-bit.
     let spec = kick_reader.spec();
     let mut writer = hound::WavWriter::create(filename, spec)?;
 
-    for sound in beat {
+    for (i, sound) in beat.iter().enumerate() {
+        
+        let is_on_beat = (i % 2) == 0;
+        
+        let target_step_length = if is_on_beat {
+            ON_BEAT_STEP_LENGTH
+        } else {
+            OFF_BEAT_STEP_LENGTH
+        };
+        
         let sound_data: &Vec<AudioSample> = match sound {
             DrumSound::Kick => &kick_samples,
             DrumSound::Snare => &snare_samples,
@@ -42,13 +56,10 @@ pub fn render_beat_to_wav(beat: &[DrumSound], filename: &str) -> Result<(), houn
             writer.write_sample(sample)?;
         }
 
-        // We must pad the rest of the "step" with silence
-        // so that each beat step has the same length.
         let samples_written = sound_data.len() as u32;
-        if samples_written < SAMPLES_PER_STEP {
-            let silence_to_add = SAMPLES_PER_STEP - samples_written;
+        if samples_written < target_step_length {
+            let silence_to_add = target_step_length - samples_written;
 
-            // `0` is silence in PCM audio.
             for _ in 0..silence_to_add {
                 writer.write_sample(0 as AudioSample)?;
             }
